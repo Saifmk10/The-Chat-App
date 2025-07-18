e15271d5d1d6920de875547c9ba824c25f5d6cc5
Saifmk10 <saifmkpvt@gmail.com>
2025-07-18 23:11:00 +0530

    UPDATE : Completed UI design for the login and signup forms , some components are still pending as of now

--- Pages/Login_Signup/loginSignup_page.tsx
+++ Pages/Login_Signup/loginSignup_page.tsx
@@ -1,51 +1,65 @@
-import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity, TextInput, SafeAreaView } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TouchableOpacity, TextInput, SafeAreaView , KeyboardAvoidingView , Platform} from "react-native";
 import LinearGradient from "react-native-linear-gradient";
-// import { Color } from "react-native/types_generated/Libraries/Animated/AnimatedExports";
+
 import LoginSignUpToogle from '../../Widgets/Login_signup/toggleButtons';
-import LoginSignUpForm  from '../../Widgets/Login_signup/loginForm';
+import LoginForm from "../../Widgets/Login_signup/loginForm";
+import SignupForm from "../../Widgets/Login_signup/signupForm";
+// import ShootingStar from "../../Widgets/Login_signup/shootingStars";
+import colors from "../../Assets/colors";
+
 
 
 const loginSignup = () => {
+
+    const [checker , setCheckerTo] = useState(true)
+
     return (
 
-        <LinearGradient colors={['#000000', '#5F48F5']} style={{ height: '100%' }} locations={[0.65, 1]}>
+        // <KeyboardAwareScrollView
+    //   style={{ flex: 1 }}
+    //   contentContainerStyle={{ flexGrow: 1 }}
+    //   enableOnAndroid={true}
+    //   keyboardShouldPersistTaps='handled'
+    // >
+
+        <LinearGradient colors={['#000000', '#5F48F5']} style={{ height: '100%'}} locations={[0.65, 1]}>
             <View style={loginSignupStyle.displayType}>
 
-                <Text style={loginSignupStyle.font}>STAY CONNECTED , FEEL CLOSER</Text>
+                {/* <Text style={loginSignupStyle.font}>STAY CONNECTED , FEEL CLOSER</Text> */}
 
                 <View style={loginSignupStyle.loginFormAndButtonParent}>
+                    
+                    <LoginSignUpToogle checker={checker} setCheckerTo={setCheckerTo}/>
 
+                    {/* <LoginForm/> */}
 
-                    <LoginSignUpToogle />
+                    {/* <SignupForm/> */}
 
-                    <LoginSignUpForm/>
+                    {checker ? <LoginForm /> : <SignupForm />}
+                    
                 </View>
+
+                <Text style={loginSignupStyle.font}>A NeW WAY TO CONNeCT</Text>
+
+                {/* <ShootingStar/> */}
 
             </View>
         </LinearGradient>
-
+        // </KeyboardAwareScrollView>
 
     )
 }
 
-const loginOrSignup = (type: String) => {
-    switch (type) {
-        case 'Signup':
-            return loginSignupStyle.loginToggleButtonsHover
-        case 'Login':
-            return loginSignupStyle.loginToggleButtonsNoHover;
-        default:
-            return loginSignupStyle.loginToggleButtonsNoHover;
-    }
-}
+
 
 const loginSignupStyle = StyleSheet.create({
 
     font: {
-        color: '#D9D9D9',
+        color: colors.secondary,
         fontSize: 40,
         fontFamily: 'Micro5-Regular',
+        paddingTop : 20
     },
 
     // holds the display type of this page
@@ -57,83 +71,10 @@ const loginSignupStyle = StyleSheet.create({
 
     },
 
-
+    // edit this property for the logical movement of the toggle widget for login form
     loginFormAndButtonParent: {
-        marginTop: 100
+        marginTop: 1
     },
-
-    // holds the login toggle design for the buttons
-    loginToggleDiv: {
-        backgroundColor: '#D9D9D9',
-        height: 55,
-        width: 300,
-        borderRadius: 10,
-        flexDirection: 'row'
-
-    },
-
-    // holds the design for the login and signup button design , when button is pressed this design is applied
-    loginToggleButtonsHover: {
-
-        backgroundColor: '#5F48F5',
-        alignItems: 'center',
-        padding: 8,
-        margin: 8,
-        borderRadius: 10,
-        borderColor: '#000000',
-        borderWidth: 2,
-
-        width: 130,
-        textAlign: 'center',
-        textAlignVertical: 'center',
-        color: '#D9D9D9',
-        fontFamily: 'Jura-Bold',
-        fontSize: 16,
-        letterSpacing: 1,
-    },
-
-    // when the button is not hover this design is applied
-    loginToggleButtonsNoHover: {
-        backgroundColor: '#D9D9D9',
-        alignItems: 'center',
-        padding: 8,
-        margin: 8,
-        borderRadius: 10,
-
-        width: 130,
-        textAlign: 'center',
-        textAlignVertical: 'center',
-        color: '#000000',
-        fontFamily: 'Jura-Bold',
-        fontSize: 16,
-        letterSpacing: 1,
-    },
-
-    // holds all the forms and buttons that are realted to the login form within this 
-    loginFormParent: {
-        backgroundColor: '#D9D9D9',
-        height: 320,
-        marginTop: 8,
-        width: 300,
-        paddingTop: 50,
-        alignItems: 'center',
-
-        borderRadius: 10
-    },
-
-    // design for the input field of the login form
-    loginFormInput: {
-        borderWidth: 3,
-        borderColor: '#000000',
-        borderRadius: 10,
-        marginBottom: 20,
-
-        color: '#000000',
-        fontFamily: 'Jura-Bold',
-        fontSize: 15,
-
-        width: 250
-    }
 
 })
 
