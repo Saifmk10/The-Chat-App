fc03d728d9be31d3b2995aa6331fe9b83a770abe
Saifmk10 <saifmkpvt@gmail.com>
2025-11-16 13:11:17 +0530

    [#3 API ERROR] - fixing the error with exception handling

--- STOCK PRICE EXTRACTION/vercelApiTest.py
+++ STOCK PRICE EXTRACTION/vercelApiTest.py
@@ -2,14 +2,14 @@ import requests
 import time  # needed for sleep
 
 # Replace this URL with your actual Vercel API endpoint (without .py)
-url1 = "https://the-chat-app-owr5pav8s-saifmks-projects.vercel.app/api/searchedapi.py?symbol=ashok%20leyland"
-url2 = "https://the-chat-app-owr5pav8s-saifmks-projects.vercel.app/api/gascraping.py"
+url1 = "https://the-chat-app-cm72p6smn-saifmks-projects.vercel.app/api/searchedapi.py?symbol=ashok%20leyland"
+url2 = "https://the-chat-app-cm72p6smn-saifmks-projects.vercel.app/api/gascraping.py"
 url3 = "https://the-chat-app-2imxqy30d-saifmks-projects.vercel.app/api/stockrecom.py"
 try:
     # while True:  # loop indefinitely
         response1 = requests.get(url1)
         response2 = requests.get(url2)
-        response3 = requests.get(url3)
+        response3 = requests.get(url3)  
         print("Status Code:", response1.status_code)
         print("Status Code:", response2.status_code)
         print("Status Code:", response3.status_code)
