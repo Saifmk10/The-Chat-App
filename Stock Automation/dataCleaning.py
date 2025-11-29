0a8913743707bd555b775f07c9abd2bc38ed1344
Saifmk10 <saifmkpvt@gmail.com>
2025-11-29 23:02:11 +0530

    [#3 ANALYTICS UPDATE] - updates the analytics section of agent with new logic (Need TO Add To B-E)

--- Stock Automation/dataCleaning.py
+++ Stock Automation/dataCleaning.py
@@ -1,18 +1,34 @@
 import pandas as pd
 
 data = pd.read_csv("input.csv")
-# print("dropped data :",data.drop(['EXTRACTEED_TIME'] , axis=0))
-print("before data cleaing :",data.head())
-print("")
-print("before data cleaing :",data.info())
-print("")
-print("before data cleaing :",data.index)
-print("")
-print("")
-print(data[["STOCK_NAME" ,  " EXTRACTED_PRICE"]])
 
-print(data[(data["EXTRACTED_TIME"] == "2025-11-21 11:02:10")])
+print("STOCK PRICE FILTERED :")
 
-# if data.isnull().any().any() : 
-#     data.dropna(axis=0 , inplace=True)
-#     print("After data cleaning :",data.values)
+def cleaningData ():
+
+    # the data gets filtered by the name 
+    filteredDataByName = data[
+        
+        data["STOCK_NAME"] == "REDINGTON"
+    ]
+    print(filteredDataByName)
+
+
+    # conveting the string data into a time stamp
+    data["EXTRACTED_TIME"] = pd.to_datetime(data["EXTRACTED_TIME"])
+
+
+    # the data gets filtered according to that particular date
+    filteredDataByDate = data[
+        (data["STOCK_NAME"] == "REDINGTON") & (data["EXTRACTED_TIME"].dt.date == pd.to_datetime("2025-11-21").date())
+    ]
+    print(filteredDataByDate)
+
+
+    # here as of now we have 3 segments of the data collected in 1 min which needs to be organized , in realworld we wont need
+    filterDataByMin = (data.set_index("EXTRACTED_TIME").groupby("STOCK_NAME").resample("1min")["EXTRACTED_PRICE"].mean().reset_index())
+    print(filterDataByMin.info())
+    filterDataByMin.to_csv("input.csv" , index=False)
+
+
+cleaningData ()
