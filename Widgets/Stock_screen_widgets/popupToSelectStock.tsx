eaa3193b21238f0ac43e04481f2761bd0e279f95
Saifmk10 <saifmkpvt@gmail.com>
2026-01-12 10:00:04 +0530

    [UPDATED MODAL] - updated the modal with search feature where the users can search for the stocks they need

--- Widgets/Stock_screen_widgets/popupToSelectStock.tsx
+++ Widgets/Stock_screen_widgets/popupToSelectStock.tsx
@@ -1,6 +1,6 @@
 // this is the screen that pops up when the user wants to add a stock or clicks on the add stock button , here the user will be able to add the stocks they needs by searching for it or by clicking on the trending stock
 
-import { Modal, View, Text, Button, TouchableOpacity, StyleSheet, TextInput, ScrollView } from 'react-native';
+import { Modal, View, Text, Button, TouchableOpacity, StyleSheet, TextInput, ScrollView , Pressable } from 'react-native';
 import colors from "D:\\PROJECTS\\The-Chat-App\\Assets\\colors.js";
 import { getAuth } from '@react-native-firebase/auth';
 import { getFirestore, collection, doc, addDoc, setDoc } from "@react-native-firebase/firestore";
@@ -9,9 +9,10 @@ import { useState } from 'react';
 
 // import { fetchingAddedStock } from './addedStocksList';
 
-const handlingSelectedStock = (pressed: any) => {
-    console.log(pressed.name)
-}
+// const handlingSelectedStock = (pressed: any) => {
+//     console.log(pressed.name)
+
+// }
 
 
 const addStockToDb = async (pressed: any) => {
@@ -45,24 +46,50 @@ const addStockToDb = async (pressed: any) => {
 
 }
 
-// function used to search for a particulat stock , native api is used
 
 
 
 
+// this is used as a function , which means all the data displayed here is coming form the addStockOptionButton
 const Popupmessage = ({ message, buttonText1, buttonText2, visible, onClose, stockArray }: { message: any, buttonText1: any, buttonText2: any, visible: any, onClose: any, stockArray: any[] }) => {
 
 
-    const [searchedStockName , setSearchedStockName] = useState("");
+    const [searched, setSearched] = useState(""); // used to store the user searched stock and then later is passed into the api for the data fetching
+    const [searchedStockName, setSearchedStockName] = useState(""); // used to fetch the searched stock name so it can be rendered in the ui later
+    const [searchedStockPrice, setSearchedStockPrice] = useState<number | null>(null); // used to fetch the price and later can be rendered
+    const [isSearched, setIsSearched] = useState(Boolean) // checks is the user has searched for any stock, if searched then the data will be shown if not then the trending stock be displayed
 
-    const searchedStock = () => {
-        
+
+    // function used to fetch the user searched stock details, done with native api
+    const searchedStock = async (searched: string) => {
+        console.log("USER SEARCHED FOR :", searched)
+
+        try {
+            const URL = `https://stock-api.saifmk.website/search/${encodeURIComponent(searched)}`
+            const request = await fetch(URL)
+            const data = await request.json()
+
+            setSearchedStockName(data.stockName)
+            setSearchedStockPrice(data.stockPrice)
+
+
+            console.log("API DATA:", data.stockName, data.stockPrice);
+        }
+        catch (error) {
+            console.log(error)
+        }
+
     }
-    
+
+
+
+
 
     return (
         // this is modal where we get the popup like feature so never remove the modal tag
         <Modal visible={visible} animationType="fade" transparent statusBarTranslucent>
+
+            <Pressable onPress={onClose} style={{flex:1 , backgroundColor: "rgba(0,0,0,0.4)",}}>
             <View style={modalStyle.mainParent}>
                 <View style={modalStyle.parentContainer}>
 
@@ -73,23 +100,42 @@ const Popupmessage = ({ message, buttonText1, buttonText2, visible, onClose, sto
 
                     {/* this is the search option */}
                     <View>
-                        <TextInput placeholder="Enter Stock Name" placeholderTextColor="#ffffff" style={modalStyle.stockInputSearch} value={searchedStockName}></TextInput>
+                        <TextInput placeholder="Enter Stock Name" placeholderTextColor="#ffffff" style={modalStyle.stockInputSearch} value={searched} onChangeText={setSearched} returnKeyType="search" onSubmitEditing={() => { searchedStock(searched), setIsSearched(true); }} />
                     </View>
 
                     <Text>TRENDING TODAY</Text>
 
                     {/* with the help of the stock array parameter passed form the addstocksoptions.tsx */}
-                    <ScrollView contentContainerStyle={modalStyle.stockListContainer}>
-                        {stockArray.map((stock, index) => (
-                            <View >
-                                <TouchableOpacity key={index} style={modalStyle.stockNameAndPrice} onPress={() => { addStockToDb(stock) }}>
-                                    <Text style={modalStyle.stockName}>{stock.name}: </Text>
-                                    <Text style={modalStyle.stockPrice}>₹{stock.price}</Text>
+
+                    {
+                        isSearched ? (
+
+                            <View style={modalStyle.searchedStockDesign}>
+                                <TouchableOpacity style={modalStyle.stockNameAndPrice} onPress={() => { addStockToDb(searchedStockName) }}>
+                                    <Text style={modalStyle.stockName}>{searchedStockName}: </Text>
+                                    <Text style={modalStyle.stockPrice}>₹{searchedStockPrice}</Text>
                                 </TouchableOpacity>
                             </View>
 
-                        ))}
-                    </ScrollView>
+                        ) :
+
+                            (
+                                <ScrollView contentContainerStyle={modalStyle.stockListContainer}>
+                                    {stockArray.map((stock, index) => (
+                                        <View >
+                                            <TouchableOpacity key={index} style={modalStyle.stockNameAndPrice} onPress={() => { addStockToDb(stock) }}>
+                                                <Text style={modalStyle.stockName}>{stock.name}: </Text>
+                                                <Text style={modalStyle.stockPrice}>₹{stock.price}</Text>
+                                            </TouchableOpacity>
+                                        </View>
+                                    ))}
+                                </ScrollView>
+                            )
+
+                    }
+
+
+
 
 
                     {/* button to close the pop up message */}
@@ -103,6 +149,7 @@ const Popupmessage = ({ message, buttonText1, buttonText2, visible, onClose, sto
                     </View>
                 </View>
             </View>
+            </Pressable>
         </Modal>
     );
 };
@@ -166,15 +213,20 @@ const modalStyle = StyleSheet.create({
         backgroundColor: "#000000",
         width: 250,
         borderRadius: 20,
+        color: "#ffff"
 
+    },
+    searchedStockDesign: {
+        justifyContent: "flex-start",
+        alignItems: "flex-start"
     },
 
     stockListContainer: {
         flexDirection: "row",
         flexWrap: "wrap",
         // justifyContent: "space-between",
-        alignItems : "center",
-        justifyContent : "center"
+        alignItems: "center",
+        justifyContent: "center"
     },
 
     stockNameAndPrice: {
@@ -184,7 +236,7 @@ const modalStyle = StyleSheet.create({
         margin: 4,
         borderRadius: 20,
         width: 150,
-        height : 100
+        height: 100
     },
     stockName: {
         color: "#ffffff",
